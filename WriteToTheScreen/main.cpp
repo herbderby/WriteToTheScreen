61aba4d72678930758fe21675c33e9dc133d9ca3
herbderby <herbderby@gmail.com>
2013-07-31 11:53:12 -0400

    Initial Commit

---
+++ WriteToTheScreen/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WriteToTheScreen
+//
+//  Created by Herb Derby on 7/31/13.
+//  Copyright (c) 2013 Herb Derby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
